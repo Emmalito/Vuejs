0ee3b916af4d8db1ab2ac9dbd14cba587679b882
Emmalito <45518759+Emmalito@users.noreply.github.com>
2019-06-10 20:26:30 +0200

    src

---
+++ App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+    <router-view name='game' v-on:score="updateScore" :user="user"/>
+    <router-view name='user' class="text-center" v-on:user="updateUser"/>
+    <router-view name='score' class="text-center" :score="score" :user="user"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {score: 0, user: false}
+  },
+  methods: {
+    updateScore: function (score) {
+      this.score = score
+    },
+    updateUser: function () {
+      this.user = true
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 500px;
+  display: block;
+}
+</style>
